aff70ba82fed593a078eda667ae66ed0d1341ad1
ced4rtree <usernamesarenoteasy@proton.me>
2026-06-01 14:58:06 -0600

    remove old files from test_custom_log_files

--- phantomwalk/tests/test_create_system_dpd.py
+++ phantomwalk/tests/test_create_system_dpd.py
@@ -13,6 +13,17 @@ def test_creation():
     assert s > 0
 
 def test_custom_log_files():
+    # remove files that might've been output by other tests so that file loading
+    # can fail if it's wrong
+    try:
+        os.remove('trajectory.gsd')
+    except:
+        pass
+    try:
+        os.remove('log.txt')
+    except:
+        pass
+
     time_string = datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
     gsd_file_name = f"{time_string}.gsd"
     log_file_name = f"{time_string}.txt"
@@ -24,3 +35,7 @@ def test_custom_log_files():
         log_file_name=log_file_name
     )
     assert os.path.isfile(gsd_file_name) and os.path.isfile(log_file_name)
+
+    # clean up after ourselves
+    os.remove(gsd_file_name)
+    os.remove(log_file_name)
